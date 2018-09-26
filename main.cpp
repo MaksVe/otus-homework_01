6062e09a8cee0a311863d11701193d140e194e92
Максим <maksim@iMac-Maksim.local>
2018-09-26 13:57:25 +0300

    first commint

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "lib.h"
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "Greetings, human!" << std::endl;
+	std::cout << "Version " << version() << std::endl;
+
+	return 0;
+}
