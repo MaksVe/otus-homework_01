96d02f8198080b06510292e1d2da4f11f8127167
MaksVe <marcospoon@gmail.com>
2018-10-08 22:36:19 +0300

    Fixing errors

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-	std::cout << "Greetings, human!" << std::endl;
-	std::cout << "Version " << version() << std::endl;
+	std::cout << "build " << version() << std::endl;
+	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
 }
